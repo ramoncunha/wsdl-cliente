cb90d281cfe4d8244f8b1bc3cd3dd89c0cac3566
Ramon Cunha <ramon.network@hotmail.com>
2020-10-12 13:07:15 -0300

    Realizando requisição ao servidor webservice

--- src/main/java/com/examplo/wsdlcliente/WsdlClienteApplication.java
+++ src/main/java/com/examplo/wsdlcliente/WsdlClienteApplication.java
@@ -1,7 +1,11 @@
 package com.examplo.wsdlcliente;
 
+import com.examplo.wsdlcliente.clients.PaisClient;
+import com.exemplo.wsdlcliente.wsdl.GetPaisResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WsdlClienteApplication {
@@ -10,4 +14,16 @@ public class WsdlClienteApplication {
 		SpringApplication.run(WsdlClienteApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner lookup(PaisClient quoteClient) {
+		return args -> {
+			String pais = "Brasil";
+
+			GetPaisResponse response = quoteClient.getPais(pais);
+			System.out.println(response.getPais().getCapital());
+			System.out.println(response.getPais().getMoeda());
+			System.out.println(response.getPais().getPopulacao());
+
+		};
+	}
 }
