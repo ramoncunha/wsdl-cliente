63e7012fa05d56375692932b0eecff4314f2db03
Ramon Cunha <ramon.network@hotmail.com>
2020-10-12 12:23:27 -0300

    Iniciando projeto

---
+++ src/main/java/com/examplo/wsdlcliente/WsdlClienteApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.wsdlcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlClienteApplication.class, args);
+	}
+
+}
