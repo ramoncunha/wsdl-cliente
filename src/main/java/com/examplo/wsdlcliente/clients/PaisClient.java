79f12f9329c07b95909b5307952f88b1a7053fea
Ramon Cunha <ramon.network@hotmail.com>
2020-10-12 13:03:53 -0300

    Criando PaisClient para requisição

--- src/main/java/com/examplo/wsdlcliente/clients/PaisClient.java
+++ src/main/java/com/examplo/wsdlcliente/clients/PaisClient.java
@@ -1,4 +1,25 @@
 package com.examplo.wsdlcliente.clients;
 
-public class PaisClient {
+import com.exemplo.wsdlcliente.wsdl.GetPaisRequest;
+import com.exemplo.wsdlcliente.wsdl.GetPaisResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+@Slf4j
+public class PaisClient extends WebServiceGatewaySupport {
+
+    public GetPaisResponse getPais(String pais) {
+        GetPaisRequest request = new GetPaisRequest();
+        request.setNome(pais);
+
+        log.info("Obtendo dados sobre: " + pais);
+
+        GetPaisResponse response = (GetPaisResponse) getWebServiceTemplate()
+            .marshalSendAndReceive("http://localhost:8080/ws/paises", request,
+                new SoapActionCallback("http://localhost:8080/wsdlservidor/assets/GetPaisRequest"));
+
+        return response;
+    }
+
 }
